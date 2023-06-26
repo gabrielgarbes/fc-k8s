1e4c279e7f3eaf9b27b4bca6e865d7fb8304babc
Gabriel Garbes <galvao.gabrielg@gmail.com>
2023-06-26 16:03:08 -0300

    aula probe

--- ambiente.sh
+++ ambiente.sh
@@ -1,4 +1,4 @@
-docker build -t gabrielgarbes/hello-go:v7.4 .
-docker push gabrielgarbes/hello-go:v7.4
+docker build -t gabrielgarbes/hello-go:v7.4.1 .
+docker push gabrielgarbes/hello-go:v7.4.1
 kubectl delete -f k8s
 kubectl apply -f k8s
--- server.go
+++ server.go
@@ -36,19 +36,18 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
-	fmt.Fprintf(w, "Hello v7.4, I'm %s. I'm %s.", name, age)
+	fmt.Fprintf(w, "Hello v7.4.1, I'm %s. I'm %s.", name, age)
 }
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 
 	duration := time.Since(startedAt)
-	fmt.Sprintf("Seconds: %v :", duration.Seconds())
-
-	if duration.Seconds() > 20 {
+	
+	if duration.Seconds() < 10 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
-	} else {
+	  } else {
 		w.WriteHeader(200)
 		w.Write([]byte("ok"))
-	}
+	  }
 }
